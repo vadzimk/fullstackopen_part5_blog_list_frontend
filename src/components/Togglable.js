d0f990bfacb9bdf897b2086593c7959ffb546bb4
Vadzim <vadzimk@hotmail.com>
2021-01-18 17:13:09 -0800

    completed 5.11,5.12 blog list frontend step11,12

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,5 +1,5 @@
 import React, {useState, useImperativeHandle} from "react";
-
+import PropTypes from 'prop-types'
 
 const Togglable = React.forwardRef((props, ref) => {
 
@@ -43,5 +43,11 @@ const Togglable = React.forwardRef((props, ref) => {
         </div>
     )
 })
+
+
+Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
+}
+Togglable.displayName = 'Togglable'
 
 export default Togglable
